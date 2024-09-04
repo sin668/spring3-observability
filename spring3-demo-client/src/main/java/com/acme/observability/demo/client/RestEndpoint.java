738dde43689588fddd31ff8c9028569df9da0f51
admin <admin@che>
2024-09-04 09:47:45 +0000

    Add span message

--- spring3-demo-client/src/main/java/com/acme/observability/demo/client/RestEndpoint.java
+++ spring3-demo-client/src/main/java/com/acme/observability/demo/client/RestEndpoint.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import io.opentelemetry.api.trace.Span;
 
 @RestController
 class RestEndpoint {
@@ -25,6 +26,11 @@ class RestEndpoint {
     String userName(@PathVariable("userId") String userId) {
         log.info("Got a request! User Id is: {}", userId);
         log.info("Calling Server. Using URL: {}", serverUrl + "/server/{userId}");
+
+        Span span = Span.current();
+
+        span.setAttribute("event", userId);
+        span.setAttribute("message", "this is a log message for userId is " + userId);
         
         return restTemplate.getForObject(serverUrl + "/server/{userId}", String.class, userId); 
     }
